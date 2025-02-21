30126f4e527691b63b5cc63603864f8529b4e4bf
Yorn90104 <bingxiao526@gmail.com>
2025-02-22 01:12:37 +0800

    Begin Function

--- src/app/game/page.jsx
+++ src/app/game/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Game from "./backend/PlayLaBaG";
 import BackButton from "./components/BackButton";
 import TitlePicture from "./components/TitlePicture";
@@ -8,6 +9,11 @@ import InfoText from "./components/InfoText";
 import BeginButton from "./components/BeginButton";
 import MusicButton from "./components/MusicButton";
 import PopPicture from "./components/PopPicture";
+
+import BG from "@/assets/BG.jpg";
+import SuperBG from "@/assets/SuperBG.jpg";
+import GreenBG from "@/assets/GreenBG.jpg";
+import KachuBG from "@/assets/KachuBG.jpg";
 
 import QST from "@/assets/QST.jpg";
 import SuperQST from "@/assets/SuperQST.jpg";
@@ -28,6 +34,17 @@ import GreenMid from "@/assets/GreenMid.jpg";
 import GreenRight from "@/assets/GreenRight.jpg";
 import pikachu from "@/assets/pikachu.jpg";
 
+import Ding from "@/assets/Ding.mp3";
+import SuperUP from "@/assets/SuperUP.mp3";
+import GreenUP from "@/assets/GreenUP.mp3";
+
+const BGs = {
+  Normal: BG,
+  SuperHHH: SuperBG,
+  GreenWei: GreenBG,
+  PiKaChu: KachuBG,
+};
+
 const QSTs = {
   Normal: QST,
   SuperHHH: SuperQST,
@@ -44,7 +61,13 @@ const CodePictures = {
   F: Rrr,
 };
 
+function Sound(s) {
+  const audio = new Audio(s);
+  audio.play();
+}
+
 export default function GamePage() {
+  const router = useRouter();
   const [IsClient, setIsClient] = useState(false); // 用於確保只在客戶端處理音樂邏輯
   const [BgmRunning, setBgmRunning] = useState(false);
   // 設置只在客戶端時運行
@@ -69,6 +92,118 @@ export default function GamePage() {
   const [GssNum, setGssNum] = useState(Game.GssNum);
   const [ModeTimes, setModeTimes] = useState(0);
 
+  // **使用 useEffect 來更新背景**
+  useEffect(() => {
+      document.body.style.backgroundImage = `url(${BGs[NowMode]})`;
+      console.log(`更換背景: ${Game.NowMode()}`);
+  }, [NowMode]); // **當 NowMode 改變時，執行 useEffect**
+
+  function Begin() {
+    const reset_qst_and_marginscore = () => {
+      setPs.forEach((setP) => {
+        setP(QSTs[Game.NowMode()]);
+      });
+      setMarginScore(0);
+      setDoubleScore(0);
+    };
+    const change_picture_per500ms = () => {
+      setPs.forEach((setP, i) => {
+        setTimeout(() => {
+          setP(CodePictures[Game.Ps[i].code]);
+          console.log(`更新位置 ${i} 的圖片`);
+          Sound(Ding);
+        }, 500 * (i + 1));
+      });
+    };
+    const mode_picture_and_sound = () => {
+      const mode = Game.NowMode();
+      switch (mode) {
+        case "Normal":
+          break;
+        case "SuperHHH":
+          for (let i = 0; i < 3; i++) {
+            if (Game.Ps[i].code == "B") {
+              setPs[i](superhhh);
+            }
+          }
+          Sound(SuperUP);
+          break;
+        case "GreenWei":
+          if (Game.Ps.every((p) => p.code === "A")) {
+            setLP(GreenLeft);
+            setMP(GreenMid);
+            setRP(GreenRight);
+          } else if (Game.Ps.some((p) => p.code === "A")) {
+            for (let i = 0; i < 3; i++) {
+              if (Game.Ps[i].code == "A") {
+                setPs[i](greenwei);
+              }
+            }
+          } else {
+            setPs.forEach((setP) => {
+              setP(greenwei);
+            });
+          }
+          Sound(GreenUP);
+          break;
+        case "PiKaChu":
+          for (let i = 0; i < 3; i++) {
+            if (Game.Ps[i].code == "E") {
+              setPs[i](pikachu);
+            }
+          }
+          break;
+      }
+    };
+    const info_text = () => {
+      setMarginScore(Game.MarginScore);
+      setDoubleScore(Game.DoubleScore);
+      setScore(Game.Score);
+      setTimes(Game.Times - Game.Played);
+      setGssNum(Game.GssNum);
+      const Mode = Game.NowMode();
+      switch (Mode) {
+        case "SuperHHH":
+          setModeTimes(Game.SuperTimes);
+          break;
+        case "GreenWei":
+          setModeTimes(Game.GreenTimes);
+          break;
+        case "PiKaChu":
+          setModeTimes(Game.KachuTimes);
+          break;
+      }
+    };
+
+    // 主要執行階段
+    setButtonAble(false);
+    reset_qst_and_marginscore();
+    Game.Logic();
+    change_picture_per500ms();
+
+    setTimeout(() => {
+      if (Game.NowMode() !== NowMode) {
+        setNowMode(Game.NowMode());
+      }
+      info_text();
+      if (Game.ModeToScreen) {
+        mode_picture_and_sound();
+      }
+    }, 3000);
+
+    setTimeout(() => {
+        if (!Game.GameRunning()) {
+          setBgmRunning(false);
+          router.push("/gameover");
+          Sound(Ding);
+        }
+        if (Game.NowMode() !== "Normal" && Game.ModeToScreen) {
+          setNowPoP(true);
+        }
+        setButtonAble(true);
+      }, 3500);
+  }
+
   return (
     <div className="GameScreen">
       <BackButton />
@@ -87,7 +222,7 @@ export default function GamePage() {
             NowMode={NowMode}
             ModeTimes={ModeTimes}
           />
-          <BeginButton BeginFunc={() => {}} Able={ButtonAble} />
+          <BeginButton BeginFunc={Begin} Able={ButtonAble} />
           <MusicButton
             IsClient={IsClient}
             BgmRunning={BgmRunning}
