408c78f4d327c0612dfcebb49ada63065a892978
Yorn90104 <bingxiao526@gmail.com>
2025-02-23 22:21:56 +0800

    GameOver Page 中的 AgainButton & InfoText

---
+++ src/app/gameover/components/InfoText.jsx
@@ -0,0 +1,21 @@
+import Game from "@/app/game/backend/PlayLaBaG";
+export default function InfoText() {
+  return (
+    <>
+      <p
+        style={{
+          fontSize: "50px",
+          color: "red",
+          textShadow:
+            "-2px -2px 0 white, 2px -2px 0 white, -2px 2px 0 white, 2px 2px 0 white",
+          margin: "0px",
+        }}
+      >
+        <b>遊戲結束！</b>
+      </p>
+      <p style={{ fontSize: "25px", color: "white" }}>
+        <b>{`最終分數為: ${Game.Score}`}</b>
+      </p>
+    </>
+  );
+}
--- src/app/gameover/page.jsx
+++ src/app/gameover/page.jsx
@@ -1,5 +1,12 @@
-export default function GameOverPage(){
-    return (
-        <></>
-    );
+"use client";
+import AgainButton from "./components/AgainButton";
+import InfoText from "./components/InfoText";
+
+export default function GameOverPage() {
+  return (
+    <div className="Screen">
+      <InfoText/>
+      <AgainButton />
+    </div>
+  );
 }
