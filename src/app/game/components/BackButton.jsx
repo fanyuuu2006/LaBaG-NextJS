f3220f95c19e19a482b627d91e9f5c7ce6637576
Yorn90104 <bingxiao526@gmail.com>
2025-02-24 01:03:28 +0800

    feat: 返回詢問

--- src/app/game/components/BackButton.jsx
+++ src/app/game/components/BackButton.jsx
@@ -1,18 +1,32 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import back from "@/assets/back.jpg";
+import Swal from "sweetalert2";
 
 export default function BackButton() {
   const router = useRouter();
+  const DialogBox = ()  => {
+    Swal.fire({
+      title: "確認是否返回首頁？",
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonColor: "#EA0000",
+      cancelButtonColor: "#0055d6",
+      confirmButtonText: "確定",
+      cancelButtonText: "取消",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        router.push("/");
+        console.log("切換至 Index 畫面");
+      }
+    });
+  }
 
   return (
     <Image
       src={back}
       alt="返回按鈕"
-      onClick={() => {
-        router.push("/");
-        console.log("切換至 Index 畫面");
-      }}
+      onClick={DialogBox}
       style={{
         cursor: "pointer",
         border: "3px solid white",
