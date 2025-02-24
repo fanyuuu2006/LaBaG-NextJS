6f445827589dff0473b00d028e4228a9deebabf6
Yorn90104 <bingxiao526@gmail.com>
2025-02-24 22:39:27 +0800

    refactor: SuperCircle 圖片組件化

---
+++ src/app/components/CirclePicture.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import SuperCircle from "@/assets/SuperCircle.png";
+
+export default function CirclePicture() {
+  const router = useRouter();
+
+  function IntoGame() {
+    Game.Reset();
+    router.push("/game");
+    console.log("切換至 Game 畫面");
+  }
+
+  return (
+    <>
+      <Image
+        src={SuperCircle}
+        alt="SuperCircle"
+        priority // 添加這行，告訴 Next.js 優先加載這張圖片
+        onClick={() => {
+          IntoGame();
+        }}
+        style={{ cursor: "pointer" }}
+      />
+    </>
+  );
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,27 +1,10 @@
 "use client";
-import Game from "./game/backend/PlayLaBaG";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-import SuperCircle from "@/assets/SuperCircle.png";
+import CirclePicture from "./components/CirclePicture";
 
 export default function IndexPage() {
-  const router = useRouter();
-
-  function IntoGame() {
-    Game.Reset();
-    router.push("/game");
-    console.log("切換至 Game 畫面");
-  }
-
   return (
     <div className="Screen">
-      <Image
-        src = {SuperCircle}
-        alt = "SuperCircle"
-        priority // 添加這行，告訴 Next.js 優先加載這張圖片
-        onClick = {() => {IntoGame()}}
-        style={{ cursor: "pointer" }}
-      />
+      <CirclePicture />
       <p
         style={{
           textAlign: "center",
