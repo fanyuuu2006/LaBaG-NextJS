9e7e2869fb78ffb9cbdb561fe9ffa2a0b182c853
Yorn90104 <bingxiao526@gmail.com>
2025-02-25 21:50:17 +0800

    refactor: BackButton => HomeButton

--- src/app/game/components/BackButton.jsx
+++ src/app/game/components/BackButton.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
+import { FaHome } from "react-icons/fa";
 import { useRouter } from "next/navigation";
-import back from "@/assets/back.jpg";
 import Swal from "sweetalert2";
 
 export default function BackButton() {
@@ -23,18 +23,16 @@ export default function BackButton() {
   }
 
   return (
-    <Image
-      src={back}
-      alt="返回按鈕"
+    <FaHome
       onClick={DialogBox}
+      size={30}
+      color="#FFFFFF"
       style={{
         cursor: "pointer",
-        border: "3px solid white",
         position: "fixed",
         top: "10px" /* 距離視窗頂部 */,
         left: "10px" /* 距離視窗左邊 */,
-        width: "30px",
-        height: "30px",
+        border: "3px solid white",
         borderRadius: "5px",
       }}
     />
