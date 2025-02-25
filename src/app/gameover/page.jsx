5a34e42ae2f8c1c368ab415b7c6dc9d617490448
Yorn90104 <bingxiao526@gmail.com>
2025-02-25 20:53:44 +0800

    feat: 下載 Game.AllData 的json檔案按鈕

--- src/app/gameover/page.jsx
+++ src/app/gameover/page.jsx
@@ -1,13 +1,14 @@
 "use client";
 import AgainButton from "./components/AgainButton";
 import InfoText from "./components/InfoText";
+import DownloadJson from "./components/DownloadJson";
 
 export default function GameOverPage() {
   return (
     <div className="Screen">
       <InfoText/>
       <AgainButton />
-
+      <DownloadJson/>
     </div>
   );
 }
