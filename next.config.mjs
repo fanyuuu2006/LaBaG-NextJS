e423fd833d10a29005b7d7f22b37ec355faf1cb9
Yorn90104 <bingxiao526@gmail.com>
2025-02-26 02:08:38 +0800

    feat: 顯示鄧入用戶頭像

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config) {
-      // 配置 Webpack 以處理音頻文件
-      config.module.rules.push({
-        test: /\.(mp3|wav|ogg)$/i,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: 'static/media/[name].[hash].[ext]', // 設定輸出的檔名格式
-          },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+  webpack(config) {
+    // 配置 Webpack 以處理音頻文件
+    config.module.rules.push({
+      test: /\.(mp3|wav|ogg)$/i,
+      use: {
+        loader: "file-loader",
+        options: {
+          name: "static/media/[name].[hash].[ext]", // 設定輸出的檔名格式
         },
-      });
-  
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-  
+      },
+    });
+
+    return config;
+  },
+};
+
+export default nextConfig;
--- src/app/components/LoginButton.jsx
+++ src/app/components/LoginButton.jsx
@@ -1,16 +1,32 @@
 import { signIn, signOut, useSession } from "next-auth/react";
-
+import Image from "next/image";
 export default function LoginButton() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
+
+  if (status === "loading")
+    return <p style={{ color: "#FFFFFF", fontWeight: "bold", left: 0 }}>載入中...</p>;
 
   return (
     <>
       {session ? (
         <>
-          <button className="LoginButton" onClick={() => signOut()}>登出</button>
+          <div style={{ display: "flex", alignItems: "center", marginInline: "20px"}}>
+            <button className="LoginButton" onClick={() => signOut()}>
+              登出
+            </button>
+            <Image
+              src={session.user.image}
+              width={40}
+              height={40}
+              style={{borderRadius: "100%", cursor: "pointer"}}
+              alt="User Avatar"
+            />
+          </div>
         </>
       ) : (
-        <button className="LoginButton" onClick={() => signIn("google")}>登入</button>
+        <button className="LoginButton" onClick={() => signIn("google")}>
+          登入
+        </button>
       )}
     </>
   );
