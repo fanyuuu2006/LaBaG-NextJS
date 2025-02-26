2fff2345e4528562806b64c7f926551977811c8e
Yorn90104 <bingxiao526@gmail.com>
2025-02-26 09:37:37 +0800

    feat: Google icon

--- src/app/components/LoginButton.jsx
+++ src/app/components/LoginButton.jsx
@@ -1,24 +1,27 @@
 import { signIn, signOut, useSession } from "next-auth/react";
-import UserButton from "./UserButton";
+import { FcGoogle } from "react-icons/fc";
 export default function LoginButton() {
   const { data: session, status } = useSession();
 
   if (status === "loading")
-    return <p style={{ color: "#FFFFFF", fontWeight: "bold", left: 0 }}>載入中...</p>;
+    return (
+      <p style={{ color: "#FFFFFF", fontWeight: "bold", left: 0 }}>載入中...</p>
+    );
 
   return (
     <>
       {session ? (
         <>
-          <div style={{ display: "flex", alignItems: "center", marginInline: "20px"}}>
-            <button className="LoginButton" onClick={() => signOut()}>
-              登出
-            </button>
-          </div>
+          <button className="LoginButton" onClick={() => signOut()}>
+            登出
+          </button>
         </>
       ) : (
         <button className="LoginButton" onClick={() => signIn("google")}>
-          登入
+          <FcGoogle
+            style={{ backgroundColor: "#FFFFFF", borderRadius: "100%" }}
+          />{" "}
+          使用 Google 帳號登入
         </button>
       )}
     </>
