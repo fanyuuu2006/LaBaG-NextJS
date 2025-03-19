457ecd45f320fad347bdd71cde77d469973766c0
Yorn90104 <bingxiao526@gmail.com>
2025-03-19 10:12:17 +0800

    feat: 用戶面板加入用戶名與郵件

--- frontend/src/component/common/AuthButton.tsx
+++ frontend/src/component/common/AuthButton.tsx
@@ -1,32 +1,33 @@
 "use client";
 import Image from "next/image";
-import { Button, Dropdown, MenuProps, Space } from "antd";
+import { Button, Space } from "antd";
 import { FcGoogle } from "react-icons/fc";
 import { signIn, signOut, useSession } from "next-auth/react";
 import ModeColors from "@/json/ModeColors.json";
 import { useNowMode } from "@/app/NowModeContext";
+import { CustomModal, Modal } from "./Alert";
 
-const AuthMenu: MenuProps = {
-  items: [
-    {
-      key: "登出",
-      label: (
-        <Button
-          onClick={() => {
-            signOut();
-          }}
-          style={{
-            backgroundColor: "#FF0000",
-            border: "#690000 solid 1px",
-            color: "#FFFFFF",
-          }}
-        >
-          登出
-        </Button>
-      ),
-    },
-  ],
-};
+// const AuthMenu: MenuProps = {
+//   items: [
+//     {
+//       key: "登出",
+//       label: (
+// <Button
+//   onClick={() => {
+//     signOut();
+//   }}
+//   style={{
+//     backgroundColor: "#FF0000",
+//     border: "#690000 solid 1px",
+//     color: "#FFFFFF",
+//   }}
+// >
+//   登出
+// </Button>
+//       ),
+//     },
+//   ],
+// };
 
 export const AuthButton = () => {
   const { data: session, status } = useSession();
@@ -36,33 +37,83 @@ export const AuthButton = () => {
       {status === "loading" ? (
         <div>載入中</div>
       ) : session ? (
-        <Dropdown
-          menu={{
-            items: AuthMenu.items,
-            style: {
-              backgroundColor: "#FFFFFF",
-              border: `${ModeColors[NowMode].dark} solid 3px`,
-            },
+        <Image
+          unoptimized={true}
+          onClick={() => {
+            CustomModal({
+              showCancelButton: true,
+              html: (
+                <Space
+                  direction="vertical"
+                  align="center"
+                  size={0}
+                  style={{
+                    backgroundColor: "#FFFFFF",
+                    border: `${ModeColors[NowMode].dark} solid 5px`,
+                    borderRadius: "10%",
+                    padding: "1em 2em",
+                  }}
+                >
+                  <Image
+                    unoptimized={true}
+                    width={10}
+                    height={10}
+                    src={session?.user?.image ?? "/default-avatar.png"}
+                    alt="頭像"
+                    style={{
+                      width: "auto",
+                      height: "4em",
+                      border: `${ModeColors[NowMode].dark} solid 2px`,
+                      borderRadius: "100%",
+                    }}
+                  />
+                  <div className="Label">{session?.user?.name}</div>
+                  <div className="Note">{session?.user?.email}</div>
+                  <div
+                    style={{
+                      marginTop: "1em",
+                      display: "flex",
+                      gap: "0.5em",
+                    }}
+                  >
+                    <Button
+                      type="primary"
+                      onClick={() => {
+                        Modal.close();
+                      }}
+                      style={{}}
+                    >
+                      關閉
+                    </Button>
+                    <Button
+                      onClick={() => {
+                        signOut();
+                      }}
+                      style={{
+                        backgroundColor: "#FF0000",
+                        border: "#690000 solid 1px",
+                        color: "#FFFFFF",
+                      }}
+                    >
+                      登出
+                    </Button>
+                  </div>
+                </Space>
+              ),
+            });
           }}
-          arrow={true}
-          placement="bottom"
-          trigger={["click"]}
-        >
-          <Image
-            unoptimized={true}
-            src={session?.user?.image ?? "/default-avatar.png"}
-            width={20}
-            height={20}
-            alt="頭像"
-            style={{
-              width: "auto",
-              height: "3em",
-              border: `${ModeColors[NowMode].light} solid 2px`,
-              borderRadius: "100%",
-              cursor: "pointer",
-            }}
-          />
-        </Dropdown>
+          src={session?.user?.image ?? "/default-avatar.png"}
+          width={20}
+          height={20}
+          alt="頭像"
+          style={{
+            width: "auto",
+            height: "3em",
+            border: `${ModeColors[NowMode].dark} solid 2px`,
+            borderRadius: "100%",
+            cursor: "pointer",
+          }}
+        />
       ) : (
         <Button
           onClick={() => {
