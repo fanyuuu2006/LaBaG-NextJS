86a00e86ce5f4a08b2ddcc4d7313b3eb5208eab5
Yorn90104 <bingxiao526@gmail.com>
2025-03-22 19:23:44 +0800

    feat: 歷史紀錄表格樣式

--- frontend/src/components/Profile/ProfileSection.tsx
+++ frontend/src/components/Profile/ProfileSection.tsx
@@ -118,45 +118,72 @@ export const ProfileSection = () => {
                 </div>
               </div>
             </Space>
-            <div className="Note">
-              歷史最高分數:{" "}
-              <span className="Label">
-                {/*補空格*/}
-                {String(
-                  HistoryDatas?.length
-                    ? Math.max(...HistoryDatas.map((h: HistoryData) => h.score))
-                    : 0
-                ).padStart(8, "\u00A0")}
-              </span>{" "}
-              分
-            </div>
-
-            <div
-              style={{
-                maxHeight: "400px", // 限制表格的最大高度
-                overflowY: "auto", // 開啟垂直滾動條
-                width: "100%",
-              }}
-            >
-              <table>
-                <thead>
-                  <tr className="Content">
-                    <th className="CenterAlign">時間</th>
-                    <th className="RightAlign">分數</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {(HistoryDatas ?? [])
-                    .slice(-20)
-                    .map((data: HistoryData, i: number) => (
-                      <tr key={i} className="Note">
-                        <td className="LeftAlign">{data.timestamp}</td>
-                        <td className="RightAlign">{data.score}</td>
+            {HistoryDatas && (
+              <>
+                <div className="Note">
+                  歷史最高分數:{" "}
+                  <span className="Label">
+                    {/*補空格*/}
+                    {String(
+                      HistoryDatas?.length
+                        ? Math.max(
+                            ...HistoryDatas.map((h: HistoryData) => h.score)
+                          )
+                        : 0
+                    ).padStart(8, "\u00A0")}
+                  </span>{" "}
+                  分
+                </div>
+                <div className="Content">歷史紀錄</div>
+                <div
+                  style={{
+                    maxHeight: "350px", // 限制表格的最大高度
+                    overflowY: "auto", // 開啟垂直滾動條
+                    width: "100%",
+                    border: "#FFFFFF solid 1px",
+                  }}
+                >
+                  <table
+                    style={{
+                      borderCollapse: "collapse",
+                      color: "#FFFFFF",
+                      width: "100%",
+                    }}
+                  >
+                    <thead
+                      style={{
+                        position: "sticky", // 使表頭固定
+                        top: 0, // 固定在頂部
+                        backgroundColor: "rgba(0, 0, 0, 0.5)", // 根據模式顏色設置背景色
+                        backdropFilter: "blur(2px)",
+                        zIndex: 1, // 確保表頭在其他內容之上
+                      }}
+                    >
+                      <tr className="Content">
+                        <th className="CenterAlign">時間</th>
+                        <th className="RightAlign">分數</th>
                       </tr>
-                    ))}
-                </tbody>
-              </table>
-            </div>
+                    </thead>
+                    <tbody>
+                      {HistoryDatas?.sort((a, b) => b.score - a.score)
+                        .slice(-20)
+                        .map((data: HistoryData, i: number) => (
+                          <tr key={i} className="Note">
+                            <td className="LeftAlign">{data.timestamp}</td>
+
+                            <td
+                              className="RightAlign"
+                              style={{ whiteSpace: "nowrap" }}
+                            >
+                              {data.score}
+                            </td>
+                          </tr>
+                        ))}
+                    </tbody>
+                  </table>
+                </div>
+              </>
+            )}
             <AuthButton
               style={{ backgroundColor: "#FF3333", marginTop: "0.5em" }}
             />
