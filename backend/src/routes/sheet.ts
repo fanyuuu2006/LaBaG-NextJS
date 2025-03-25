2eee3559be286dcc224288b618eb433a274d03b1
Yorn90104 <bingxiao526@gmail.com>
2025-03-25 23:51:51 +0800

    fix: addUser s

--- backend/src/routes/sheet.ts
+++ backend/src/routes/sheet.ts
@@ -5,5 +5,5 @@ import { authMiddleware } from "../middlewares/authMiddleware";
 export const router = Router();
 
 router.get("/getUsers", getUsers);
-router.post("/addUsers", authMiddleware, addUser);
+router.post("/addUser", authMiddleware, addUser);
 router.get("/getRecords", getRecords);
