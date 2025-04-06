cc743fb7f4d85e23ef7c24516e709059ac0b7fcc
Yorn90104 <bingxiao526@gmail.com>
2025-04-06 16:05:02 +0800

    feature: LaBaG npm 模組

--- LaBaG/src/types/JsonLaBaG.ts
+++ LaBaG/src/types/JsonLaBaG.ts
@@ -1,6 +1,6 @@
-import { Modes } from "./Mode";
 import { P } from "./P";
 import { BaseLaBaG } from "./LaBaG";
+import { Modes } from "..";
 
 export class JsonLaBaG extends BaseLaBaG {
   jsonData: BaseLaBaG["AllData"];
