a51529656f53b211bd36ad291ff21a8a5bda1d95
Yorn90104 <bingxiao526@gmail.com>
2025-04-06 16:03:37 +0800

    feature: LaBaG npm 模組

--- backend/src/middlewares/rateLimitMiddleware.ts
+++ backend/src/middlewares/rateLimitMiddleware.ts
@@ -11,5 +11,5 @@ export const recordsPostRateLimit = rateLimit({
   windowMs: 90 * 1000, // 1.5 分鐘
   max: 1,
   message: { message: "請求過於頻繁" },
-  keyGenerator: (req) => req?.ip ?? "", // 使用來自每個請求的 req.ip（即客戶端的 IP 地址）來生成唯一的識別鍵 這樣系統就會對每個 IP 地址進行獨立的限流控制
+  keyGenerator: (req) => req?.ip ?? "",
 });
