d01f860775ad6890a31fdad61f903b31101393c0
Yorn90104 <bingxiao526@gmail.com>
2025-04-08 11:16:46 +0800

    feature: PWA 主題顏色黑色

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -75,7 +75,7 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#317EFB" />
+        <meta name="theme-color" content="#000000" />
       </head>
       <body>
         <UserProvider>
