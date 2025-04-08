f79a8bd3952a214b8a84a7896cf49f8436fca4ef
Yorn90104 <bingxiao526@gmail.com>
2025-04-08 10:57:22 +0800

    feature: PWA 設定

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,16 +2,6 @@ import withPWA from 'next-pwa';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(mp3|wav|ogg)$/i,
-      type: 'asset/resource',
-      generator: {
-        filename: 'static/media/[name].[hash][ext]',
-      },
-    });
-    return config;
-  },
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
