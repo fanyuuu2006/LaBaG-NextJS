acb55a88d57f5fdfd5f554e11975a48934b68656
Yorn90104 <bingxiao526@gmail.com>
2025-04-13 15:40:49 +0800

    feature: 進入遊戲按鈕 hover樣式

--- frontend/src/components/Home/HomeSection.tsx
+++ frontend/src/components/Home/HomeSection.tsx
@@ -1,11 +1,12 @@
 import { useRouter } from "next/navigation";
-import { Button, Space, Tooltip } from "antd";
+import { Button, Space } from "antd";
 import Image from "next/image";
 import SuperCircle from "@/assets/SuperCircle.png";
 import { useNowMode } from "@/context/NowModeContext";
 import { Game } from "@/utils/game";
 import { InstallPWAButton } from "./InstallPWAButton";
 import { ModeColors } from "@/utils/ModeColors";
+import { StateStylesComponent } from "fanyucomponents";
 
 export const HomeSection = () => {
   const router = useRouter();
@@ -15,25 +16,29 @@ export const HomeSection = () => {
     <section style={{ height: "100%" }}>
       <Space direction="vertical" align="center" size={"large"}>
         <Image priority src={SuperCircle} alt="SuperCircle" />
-        <Tooltip title="點擊進入遊戲">
-          <Button
-            type="text"
-            className="Label"
-            style={{
-              backgroundColor: "rgba(0, 0 ,0, 0.5)",
-              color: "#FFFFFF",
-              border: `${ModeColors[NowMode].dark} solid 3px`,
-              padding: "1em",
-            }}
-            onClick={() => {
-              Game.Reset();
-              setNowMode(Game.NowMode());
-              router.push("./Game");
-            }}
-          >
-            進入遊戲
-          </Button>
-        </Tooltip>
+        <StateStylesComponent
+          as={Button}
+          type="text"
+          className="Label"
+          style={{
+            backgroundColor: "rgba(0, 0 ,0, 0.5)",
+            color: "#FFFFFF",
+            border: `${ModeColors[NowMode].dark} solid 3px`,
+            padding: "1em",
+          }}
+          styles={{
+            hover: {
+              filter: `drop-shadow(0 0 5px ${ModeColors[NowMode].light})`,
+            },
+          }}
+          onClick={() => {
+            Game.Reset();
+            setNowMode(Game.NowMode());
+            router.push("./Game");
+          }}
+        >
+          進入遊戲
+        </StateStylesComponent>
         <InstallPWAButton />
       </Space>
     </section>
