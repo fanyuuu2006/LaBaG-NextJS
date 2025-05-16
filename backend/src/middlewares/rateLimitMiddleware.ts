d9eb4e86a36c5dc6ac6222009864c0be392ab698
Yorn90104 <bingxiao526@gmail.com>
2025-05-16 09:15:55 +0800

    revert: 使用SWR

--- backend/src/middlewares/rateLimitMiddleware.ts
+++ backend/src/middlewares/rateLimitMiddleware.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const globalRateLimit = rateLimit({
   windowMs: 60 * 1000, // 1 分鐘
-  max: 40,
+  max: 80,
   message: { message: "請求過於頻繁" },
   keyGenerator: (req) => req?.ip ?? "", // 使用來自每個請求的 req.ip（即客戶端的 IP 地址）來生成唯一的識別鍵 這樣系統就會對每個 IP 地址進行獨立的限流控制
 });
